a28eb4f1840567a4adaac640e132f342b68ef639
Peter Bastyi <baspeti@gmail.com>
2016-11-07 16:17:30 +0100

    Make sure that Travis could query the latest package for each required package

--- rpm-build/freeze-requirement.sh
+++ rpm-build/freeze-requirement.sh
@@ -15,9 +15,15 @@ PACKAGE_NAME=$1
 TARGET_SPEC_FILE=$2
 
 # Query the latest RPM version number of the specified package
-LATEST_VERSION_NOARCH=$(curl https://rpm.palette-software.com/centos/dev/noarch/ | grep ${PACKAGE_NAME} | sed -e"s/.*${PACKAGE_NAME}-[v]*\([0-9.-]*\)\.noarch.*/\1/g")
-LATEST_VERSION_X86_64=$(curl https://rpm.palette-software.com/centos/dev/x86_64/ | grep ${PACKAGE_NAME} | sed -e"s/.*${PACKAGE_NAME}-[v]*\([0-9.-]*\)\.x86_64.*/\1/g")
+LATEST_VERSION_NOARCH=$(curl https://rpm.palette-software.com/centos/dev/noarch/ | grep ${PACKAGE_NAME} | sed -nr "s/.*$PACKAGE_NAME-[v]?([0-9.-]+)\.noarch.*/\1/p")
+LATEST_VERSION_X86_64=$(curl https://rpm.palette-software.com/centos/dev/x86_64/ | grep ${PACKAGE_NAME} | sed -nr "s/.*$PACKAGE_NAME-[v]?([0-9.-]+)\.x86_64.*/\1/p")
 LATEST_VERSION=$(echo -e "${LATEST_VERSION_NOARCH}\n${LATEST_VERSION_X86_64}" | sort -V | tail -1)
+
+if [ -z "${LATEST_VERSION}" ]; then
+	echo "Failed to query latest version of package: '${PACKAGE_NAME}'!"
+	exit 1
+fi
+
 # Replace the requirement line in the specified spec file
 # For example this script would turn a line like this
 #   Requires: palette-insight-agent
